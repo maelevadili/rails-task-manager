194496926deb34a606cdeb7f8edb27522b06f38c
maelladegras <mdegras@myrezapp.com>
2019-05-28 22:28:42 +0200

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,13 @@ Rails.application.routes.draw do
   # tjrs mettre le "new" avt le "show", sinon erreur
   # va considerer que "new" est un id...
 
+  get 'tasks/:id',      to: 'tasks#show', as: :task
+
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   # pour afficher un formulaire d'edition
-  get 'tasks/:id/mark_as_done', to: 'tasks#mark_as_done', as: :mark_task_as_done
-  get 'tasks/:id',      to: 'tasks#show', as: :task
 
   patch 'tasks/:id',    to: 'tasks#update'
   # pour enregistrer la modif en base
-  patch 'tasks/:id',    to: 'tasks#mark_as_done'
 
   delete 'tasks/:id',   to: 'tasks#destroy'
 end
